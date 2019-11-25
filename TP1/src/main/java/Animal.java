4dad9a54504243bdcf677e73e2138e2f84996ef1
Abdou <abdouzozo.14@gmail.com>
2019-11-25 16:38:44 +0100

    TP1 v1.0

---
+++ TP1/src/main/java/Animal.java
@@ -0,0 +1,10 @@
+
+abstract public class Animal {
+	
+	protected String nomAnimal;
+	protected String typeAnimal;
+	
+	
+	abstract public String getNomAnimal();
+	abstract public String getTypeAnimal() ;
+}
---
+++ TP1/src/main/java/Chien.java
@@ -0,0 +1,16 @@
+
+public class Chien extends Animal {
+
+	@Override
+	public String getNomAnimal() {
+		// TODO Auto-generated method stub
+		return "Chien";
+	}
+
+	@Override
+	public String getTypeAnimal() {
+		// TODO Auto-generated method stub
+		return typeAnimal;
+	}
+
+}
